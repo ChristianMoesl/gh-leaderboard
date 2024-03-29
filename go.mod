16572f333b7e23b518d35926273f7921b58de0fb
Christian Mösl <christian.moesl@live.at>
2024-03-29 21:36:53 +0100

    feat: fetch and process review data

---
+++ go.mod
@@ -0,0 +1,31 @@
+module github.com/ChristianMoesl/gh-leaderboard
+
+go 1.21
+
+toolchain go1.21.8
+
+require (
+	github.com/gofri/go-github-ratelimit v1.1.0
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-openapi/errors v0.22.0 // indirect
+	github.com/go-openapi/strfmt v0.23.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	go.mongodb.org/mongo-driver v1.14.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
+
+require (
+	github.com/cli/go-gh v1.2.1
+	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/google/go-github/v60 v60.0.0
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ leaderboard.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/cli/go-gh/pkg/auth"
+	"github.com/gofri/go-github-ratelimit/github_ratelimit"
+	"github.com/jedib0t/go-pretty/table"
+
+	"github.com/google/go-github/v60/github"
+)
+
+func fetchRepositories(client *github.Client, options *Options, clientNumber int, totalClients int, repos chan *github.Repository) {
+	page := clientNumber
+	for i := 0; true; i++ {
+		opts := github.RepositoryListByOrgOptions{
+			ListOptions: github.ListOptions{
+				Page: page,
+			},
+		}
+		fetchedRepos, response, err := client.Repositories.ListByOrg(context.Background(), options.Organization, &opts)
+		if err != nil {
+			panic(err)
+		}
+		for _, fetchedRepo := range fetchedRepos {
+			repos <- fetchedRepo
+		}
+
+		page = clientNumber + i*totalClients
+		if page > response.LastPage {
+			return
+		}
+	}
+}
+
+func processPullRequest(client *github.Client, options *Options, repo *github.Repository, pr *github.PullRequest, stats chan *Stats) {
+	stats <- &Stats{
+		Name:         pr.GetUser().GetLogin(),
+		PullRequests: 1,
+	}
+
+	slog.Debug("Fetching reviews", "repository", repo.GetName(), "pullRequest", "#"+strconv.Itoa(pr.GetNumber()))
+	reviews, response, err := client.PullRequests.ListReviews(context.Background(), repo.GetOwner().GetLogin(), repo.GetName(), pr.GetNumber(), &github.ListOptions{
+		PerPage: 100,
+	})
+	if err != nil {
+		panic(err)
+	}
+	if response.NextPage != 0 {
+		log.Fatalf("Found to many reviews in pull request %s/%s#%d to handle\n", repo.GetOwner().GetLogin(), repo.GetName(), pr.GetNumber())
+	}
+
+	for _, review := range reviews {
+		if review.GetSubmittedAt().After(options.Since) {
+			body := review.GetBody()
+			lines := len(strings.Split(body, "\n"))
+			stats <- &Stats{
+				Name:                review.GetUser().GetLogin(),
+				Reviews:             1,
+				CommentLinesWritten: lines,
+			}
+		}
+	}
+
+	slog.Debug("Fetching comments", "repository", repo.GetName(), "pullRequest", "#"+strconv.Itoa(pr.GetNumber()))
+	page := 0
+	for {
+		comments, response, err := client.PullRequests.ListComments(context.Background(), repo.GetOwner().GetLogin(), repo.GetName(), pr.GetNumber(), &github.PullRequestListCommentsOptions{
+			ListOptions: github.ListOptions{
+				Page: page,
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, comment := range comments {
+			if comment.GetCreatedAt().After(options.Since) {
+				body := comment.GetBody()
+				lines := len(strings.Split(body, "\n"))
+				stats <- &Stats{
+					Name:                comment.GetUser().GetLogin(),
+					Comments:            1,
+					CommentLinesWritten: lines,
+				}
+			}
+		}
+
+		if response.NextPage == 0 {
+			return
+		}
+		page = response.NextPage
+	}
+}
+
+func processRepository(client *github.Client, options *Options, repo *github.Repository, stats chan *Stats) {
+	slog.Debug("Fetch pull requests", "repository", repo.GetName())
+	page := 0
+	for {
+		pullRequests, response, err := client.PullRequests.List(context.Background(), repo.GetOwner().GetLogin(), repo.GetName(), &github.PullRequestListOptions{
+			State:     "all",
+			Sort:      "updated",
+			Direction: "desc",
+			ListOptions: github.ListOptions{
+				Page: page,
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		var wg sync.WaitGroup
+		for _, pullRequest := range pullRequests {
+			if pullRequest.GetUpdatedAt().After(options.Since) {
+				wg.Add(1)
+				go func(pr *github.PullRequest) {
+					defer wg.Done()
+					processPullRequest(client, options, repo, pr, stats)
+				}(pullRequest)
+			}
+		}
+		wg.Wait()
+
+		if response.NextPage == 0 {
+			return
+		}
+		page = response.NextPage
+	}
+}
+
+type Stats struct {
+	Name                string
+	PullRequests        int
+	Reviews             int
+	Comments            int
+	CommentLinesWritten int
+}
+
+func processRepositories(client *github.Client, options *Options, repos chan *github.Repository, stats chan *Stats) {
+	fmt.Printf("Processing data since %v matching repository name pattern %s", options.Since, options.NamePattern)
+
+	var wg sync.WaitGroup
+	for repo := range repos {
+		matched, err := regexp.MatchString(options.NamePattern, repo.GetName())
+		if err != nil {
+			panic(err)
+		}
+		if matched {
+			wg.Add(1)
+			go func(repo *github.Repository) {
+				defer wg.Done()
+				processRepository(client, options, repo, stats)
+			}(repo)
+		}
+	}
+	wg.Wait()
+	close(stats)
+}
+
+func createClient() *github.Client {
+	host, _ := auth.DefaultHost()
+	authToken, _ := auth.TokenForHost(host)
+	if authToken == "" {
+		log.Fatalf("authentication token not found for host %s", host)
+	}
+
+	rateLimiter, err := github_ratelimit.NewRateLimitWaiterClient(nil)
+	if err != nil {
+		panic(err)
+	}
+	return github.NewClient(rateLimiter).WithAuthToken(authToken)
+}
+
+func accumulateStatsPerUser(stats chan *Stats) map[string]*Stats {
+	acc := make(map[string]*Stats)
+
+	for s := range stats {
+		userStats := acc[s.Name]
+		if userStats == nil {
+			userStats = &Stats{
+				Name: s.Name,
+			}
+			acc[userStats.Name] = userStats
+		}
+
+		userStats.PullRequests += s.PullRequests
+		userStats.Reviews += s.Reviews
+		userStats.Comments += s.Comments
+		userStats.CommentLinesWritten += s.CommentLinesWritten
+	}
+
+	return acc
+}
+
+func fetchAllRepositories(client *github.Client, options *Options, repos chan *github.Repository, totalClients int) {
+	wg := sync.WaitGroup{}
+	for i := 0; i < totalClients; i++ {
+		slog.Debug("Spawned repository fetch client", "client", i)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fetchRepositories(client, options, i, totalClients, repos)
+		}(i)
+	}
+	wg.Wait()
+	close(repos)
+}
+
+func initializedLogger() {
+	input := os.Getenv("LOG_LEVEL")
+
+	var logLevel slog.Leveler
+	switch input {
+	case "DEBUG":
+		logLevel = slog.LevelDebug
+	case "WARN":
+		logLevel = slog.LevelWarn
+	case "ERROR":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+	slog.SetDefault(logger)
+}
+
+type Options struct {
+	Since        time.Time
+	Organization string
+	NamePattern  string
+}
+
+func parseCliArgs() *Options {
+	oneWeek := time.Duration(24*7) * time.Hour
+	lastWeek := time.Now().Add(-oneWeek)
+	sinceDefault := fmt.Sprintf("%d-%02d-%02d", lastWeek.Year(), lastWeek.Month(), lastWeek.Day())
+
+	var organization string
+	var namePattern string
+	var sinceText string
+	flag.StringVar(&organization, "org", "", "Github organization to scan")
+	flag.StringVar(&namePattern, "name", "*", "Regex pattern to match the repository name")
+	flag.StringVar(&sinceText, "since", sinceDefault, "time span to look into")
+	flag.Parse()
+
+	since, err := time.Parse("2006-01-02", sinceText)
+	if err != nil {
+		panic(err)
+	}
+
+	if organization == "" {
+		fmt.Println("Please provide an Github organization name with --org")
+		os.Exit(1)
+	}
+
+	return &Options{
+		Since:        since,
+		Organization: organization,
+		NamePattern:  namePattern,
+	}
+}
+
+func showResults(statsPerUser map[string]*Stats) {
+	t := table.NewWriter()
+	t.SetTitle("Code Review Leaderboard")
+	t.AppendHeader(table.Row{"User", "Pull Requests", "Reviews", "Comments", "#Comment Lines"})
+
+	for _, stats := range statsPerUser {
+		t.AppendRow(table.Row{stats.Name, stats.PullRequests, stats.Reviews, stats.Comments, stats.CommentLinesWritten})
+	}
+
+	fmt.Println(t.Render())
+}
+
+func main() {
+	initializedLogger()
+
+	options := parseCliArgs()
+
+	client := createClient()
+
+	repos := make(chan *github.Repository, 128)
+	go fetchAllRepositories(client, options, repos, 5)
+
+	stats := make(chan *Stats, 128)
+	go processRepositories(client, options, repos, stats)
+
+	accumulated := accumulateStatsPerUser(stats)
+
+	showResults(accumulated)
+}
